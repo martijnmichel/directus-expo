010e3782da5d91cdfcbb3de121b8f3ccf2984dac
martijnmichel <martijn.michel@gmail.com>
2025-01-21 09:30:57 +0100

    rm/check button

--- components/content/filters/search-filter-modal.tsx
+++ components/content/filters/search-filter-modal.tsx
@@ -7,8 +7,9 @@ import { useTranslation } from "react-i18next";
 import { Modal } from "@/components/display/modal";
 import { Button } from "@/components/display/button";
 import { DirectusIcon } from "@/components/display/directus-icon";
-import { Vertical } from "@/components/layout/Stack";
+import { Horizontal, Vertical } from "@/components/layout/Stack";
 import { UseDocumentsFiltersReturn } from "@/hooks/useDocumentsFilters";
+import { Check, X } from "@/components/icons";
 
 export const SearchFilter = (state: UseDocumentsFiltersReturn) => {
   const [search, setSearch] = useState(state.search);
@@ -30,9 +31,31 @@ export const SearchFilter = (state: UseDocumentsFiltersReturn) => {
         </Button>
       </Modal.Trigger>
       <Modal.Content variant="quickView" title={t("components.table.search")}>
-        <Vertical>
-          <Input value={search} onChangeText={setSearch} placeholder="Search" />
-        </Vertical>
+        {({ close }) => (
+          <Vertical>
+            <Input
+              value={search}
+              onChangeText={setSearch}
+              append={<DirectusIcon name="search" />}
+              placeholder="Search"
+            />
+            <Horizontal style={{ justifyContent: "flex-end" }}>
+              {!!search && (
+                <Button
+                  rounded
+                  variant="soft"
+                  onPress={() => (setSearch(""), close())}
+                >
+                  <X />
+                </Button>
+              )}
+
+              <Button rounded onPress={close}>
+                <Check />
+              </Button>
+            </Horizontal>
+          </Vertical>
+        )}
       </Modal.Content>
     </Modal>
   );
