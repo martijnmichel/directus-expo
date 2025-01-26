8cc1602a1a3f8c1861726a442c613a0c9af95990
martijnmichel <martijn.michel@gmail.com>
2025-01-26 12:42:05 +0100

    rm track path

--- app/(app)/(tabs)/files/_layout.tsx
+++ app/(app)/(tabs)/files/_layout.tsx
@@ -1,12 +1,3 @@
-import { Button } from "@/components/display/button";
-import { Modal } from "@/components/display/modal";
-import { Plus } from "@/components/icons";
-import { ImageInput } from "@/components/interfaces/image-input";
-import { BackButton } from "@/components/layout/BackButton";
-import { Horizontal } from "@/components/layout/Stack";
-import { isActionAllowed } from "@/helpers/permissions/isActionAllowed";
-import { useTrackPath } from "@/hooks/useTrackPath";
-import { usePermissions } from "@/state/queries/directus/core";
 import { useHeaderStyles } from "@/unistyles/useHeaderStyles";
 import { Stack } from "expo-router";
 
