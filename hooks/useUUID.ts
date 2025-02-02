5c9bd1a96e943e6284431305707cc5378c67572b
martijnmichel <martijn.michel@gmail.com>
2025-02-02 17:53:30 +0100

    Update useUUID.ts

--- hooks/useUUID.ts
+++ hooks/useUUID.ts
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 export const useUUID = () => {
   const uuid = useRef(uuidv4());
