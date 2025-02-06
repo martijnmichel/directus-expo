89f492b1e5e73e3005abb0070aa6051257fe73e9
martijnmichel <martijn.michel@gmail.com>
2025-02-06 11:02:52 +0100

    update website

--- website/components/swiper-screenshots.tsx
+++ website/components/swiper-screenshots.tsx
@@ -4,60 +4,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import Image from "next/image";
+import { images } from "@/screenshots";
 
 export default function SwiperScreenshots() {
-  const images = [
-    {
-      src: "/carousel/collection-data-table.png",
-      alt: "Carousel Image 1",
-      sort: 101,
-    },
-
-    {
-      src: "/carousel/collection-layout.png",
-      alt: "Carousel Image 1",
-    },
-
-    {
-      src: "/carousel/color-picker.png",
-      alt: "Carousel Image 1",
-      sort: 100,
-    },
-    {
-      src: "/carousel/content-screen.png",
-      alt: "Carousel Image 1",
-      sort: 1,
-    },
-    {
-      src: "/carousel/date-picker.png",
-      alt: "Carousel Image 1",
-    },
-    {
-      src: "/carousel/files-screen.png",
-      alt: "Carousel Image 1",
-      sort: 5,
-    },
-    {
-      src: "/carousel/m2a-page.png",
-      alt: "Carousel Image 1",
-    },
-    {
-      src: "/carousel/presentation-notice.png",
-      alt: "Carousel Image 1",
-    },
-    {
-      src: "/carousel/related-item-modal.png",
-      alt: "Carousel Image 1",
-    },
-    {
-      src: "/carousel/selection-inputs.png",
-      alt: "Carousel Image 1",
-    },
-    {
-      src: "/carousel/settings-screen.png",
-      alt: "Carousel Image 1",
-    },
-  ];
   return (
     <Swiper
       loop
