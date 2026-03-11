d52fa28ac408458ad68840c6b78f12566fe14afa
martijnmichel <martijn.michel@gmail.com>
2026-03-11 10:56:02 +0100

    feat: m2o relational

--- app/(app)/modals/m2o/[collection]/pick.tsx
+++ app/(app)/modals/m2o/[collection]/pick.tsx
@@ -37,6 +37,8 @@ import { FloatingToolbar } from "@/components/display/floating-toolbar";
 import { Pagination } from "@/components/content/filters/pagination";
 import { SearchFilter } from "@/components/content/filters/search-filter-modal";
 import { useCollectionTableFields } from "@/hooks/useCollectionTableFields";
+import { usePrimaryKey } from "@/hooks/usePrimaryKey";
+import { DataTableColumn } from "@/components/content/DataTableColumn";
 
 type PickModalParams = {
   collection: keyof CoreSchema;
@@ -57,6 +59,13 @@ export default function Collection() {
   const preset = presets?.find((p) => p.collection === collection);
 
   const { directus } = useAuth();
+
+  const primaryKey = usePrimaryKey(collection as keyof CoreSchema);
+
+  const tableFields = useCollectionTableFields({
+    collection: collection as keyof CoreSchema,
+  });
+
   const { data: options, refetch } = useDocuments(
     collection as keyof CoreSchema,
     {
@@ -65,13 +74,23 @@ export default function Collection() {
       page,
       limit,
       search,
-    }
+    },
   );
 
-  const tableFields = useCollectionTableFields({
-    collection: collection as keyof CoreSchema,
-    documents: options?.items,
-  });
+  const { data: relatedDocuments, refetch: refetchRelatedDocuments } =
+    useDocuments(collection as keyof CoreSchema, {
+      fields: [
+        ...tableFields.filter((f: string) => f.includes(".")),
+        primaryKey as any,
+      ],
+      limit: -1,
+      filter: {
+        [primaryKey as any]: {
+          _in: map(options?.items, (doc) => doc[primaryKey as string]),
+        },
+      },
+    });
+
   const headerStyles = useHeaderStyles({ isModal: true });
   const { label } = useFieldMeta(collection as keyof CoreSchema);
   const { bottom } = useSafeAreaInsets();
@@ -88,8 +107,8 @@ export default function Collection() {
       <Table
         headers={reduce(
           tableFields,
-          (prev, curr) => ({ ...prev, [curr]: label(curr) || "" }),
-          {}
+          (prev, curr) => ({ ...prev, [curr]: label(curr.split(".")[curr.split(".").length - 1]) || "" }),
+          {},
         )}
         toolbarItems={
           <>
@@ -101,7 +120,22 @@ export default function Collection() {
         items={(options?.items as Record<string, unknown>[]) || []}
         widths={preset?.layout_options?.tabular?.widths}
         renderRow={(doc) =>
-          map(tableFields, (f) => doc[f] as number | string | null)
+          map(tableFields, (f) => {
+            const relatedDoc = (
+              relatedDocuments?.items as Record<string, unknown>[] | undefined
+            )?.find(
+              (r) => r[primaryKey as string] === doc[primaryKey as string],
+            );
+            return (
+              <DataTableColumn
+                template={f}
+                document={doc}
+                relatedDocument={relatedDoc}
+                collection={collection as keyof CoreSchema}
+                key={`table-${collection}-column-${f}`}
+              />
+            );
+          })
         }
         onRowPress={(doc) => {
           router.dismiss();
